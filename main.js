3353ac394c141bdc63f093c87b08304d408cf0c5
Thiago Emanoel <thiagoemanoel448@gmail.com>
2022-05-11 16:18:06 -0300

    Aula 4

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-// navigation = objeto
+window.addEventListener('scroll', onScroll);
+
+// Clean code: 1 função - 1 coisa (Clean Code)
+
+// Gerencia os scrolls da pag
+onScroll();
 function onScroll() {
+    showNavOnScroll();
+    showBackToTopButtonOnScroll();
+}
+
+function showNavOnScroll() {
     if(scrollY > 0){
         navigation.classList.add('scroll');
     }else{
         navigation.classList.remove('scroll');
     }
 }
+
+function showBackToTopButtonOnScroll(){
+    if(scrollY > 500){
+        backToTopButton.classList.add('show');
+    }else{
+        backToTopButton.classList.remove('show');
+    }
+}
+
 
 function openMenu(){
     document.body.classList.add('menu-expanded');
